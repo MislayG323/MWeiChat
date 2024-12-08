67def6512f09769a7c647d546caef25473c1b656
Mislay <1592371845@qq.com>
2024-12-08 20:49:32 +0800

    修复:home模块编译问题

--- app/src/main/java/com/example/mweichat/MainActivity.kt
+++ app/src/main/java/com/example/mweichat/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.home.HomeScreen
 import com.example.mweichat.ui.theme.MWeiChatTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +34,73 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MWeiChatTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold (
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        }
+
+    ) { innerPadding ->
+        NavHost(
+               navController = navController,
+            startDestination = "home",
+            Modifier.padding(innerPadding)
+                ) {
+            composable("home") { HomeScreen() }
+            composable("contacts") { Text(text = "contacts") }
+            composable("discover") { Text(text = "discover") }
+            composable("profile") { Text(text = "profile") }
+        }
+    }
 }
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        BottomNavItem("home", "首页", Icons.Default.Home),
+        BottomNavItem("contacts", "通讯录", Icons.Default.Person),
+        BottomNavItem("discover", "发现", Icons.Default.Search),
+        BottomNavItem("profile", "我的", Icons.Default.Person)
+    )
+
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach { item ->
+            NavigationBarItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { Text(item.title) }
+            )
+        }
+    }
+}
+
+data class BottomNavItem(val route: String, val title: String, val icon: ImageVector)
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MWeiChatTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
